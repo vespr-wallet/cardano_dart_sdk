f33d2e23320ba2983a102eabe26ff64fb77b95c8
Alexandru Dochioiu <alex@dochioiu.com>
2025-11-03 18:49:29 +0200

    [CHORE] Update Squadron to 7.2.0 and Flutter to 3.35.7, bump version to 2.6.0

--- lib/workers/wallet_tasks.web.g.dart
+++ lib/workers/wallet_tasks.web.g.dart
@@ -2,8 +2,8 @@
 // dart format width=80
 
 // **************************************************************************
-// Generator: WorkerGenerator 8.0.0 (Squadron 7.1.2+1)
-// Generated: 2025-11-03 16:41:46.513553Z
+// Generator: WorkerGenerator 8.0.0+1 (Squadron 7.2.0)
+// Generated: 2025-11-03 16:48:31.542157Z
 // **************************************************************************
 
 import 'package:squadron/squadron.dart';
